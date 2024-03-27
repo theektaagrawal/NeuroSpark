1aefa21cf486accadcbb7f42fb6b498590d7f5b7
programmingninjas <programing.ninja0@gmail.com>
2024-03-28 04:22:07 +0530

    Added AI Power to NPC

--- client/src/components/Metaverses/Cafe.tsx
+++ client/src/components/Metaverses/Cafe.tsx
@@ -1,8 +1,10 @@
 import { Environment, useGLTF } from "@react-three/drei";
 import { useFrame, RootState } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimationMixer, Object3D } from "three";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import { useSpeech } from "react-text-to-speech";
+import {GoogleGenerativeAI} from "@google/generative-ai"
 
 function Cafe(props:{micActive?:boolean})
 {
@@ -10,6 +12,7 @@ function Cafe(props:{micActive?:boolean})
     const globalMixer = useRef<AnimationMixer>();
     const NPCMixer = useRef<AnimationMixer>();
     const AnimCamera = useRef<Object3D>();
+    const [tts, setTTS] = useState<string>("");
 
     const NPCClips = ["Armature"]
     const playAnim = useRef(false);
@@ -53,6 +56,18 @@ function Cafe(props:{micActive?:boolean})
         PlayAnimation();
     }, [gltf])
 
+    const { transcript,resetTranscript } = useSpeechRecognition();
+    const {
+        //Text, // Component that returns the modified text property
+        //speechStatus, // String that stores current speech status
+        //isInQueue, // Boolean that stores whether a speech utterance is either being spoken or present in queue
+        start, // Function to start the speech or put it in queue
+        //pause, // Function to pause the speech
+        //stop, // Function to stop the speech or remove it from queue
+      } = useSpeech({ text: tts,lang:"en-IN",
+        voiceURI:"Microsoft Heera - English (India)",});
+    const genAI = new GoogleGenerativeAI("AIzaSyBSxXpveJAOfTa7tx7BVqs53PK46unfprM");
+
     useEffect(()=>{
         console.log(props.micActive)
         if(props.micActive)
@@ -66,10 +81,30 @@ function Cafe(props:{micActive?:boolean})
             PlayAnimation();
             SpeechRecognition.stopListening();
 
+            async function GenerateResponse(input:string)
+            {
+                const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+                const result = await model.generateContent("You are a friendly and patient waitress at a family restaurant. A young child with Down Syndrome is sitting at a table with their family. You want to make sure the child feels comfortable and understood, so you use simple language, repeat things if necessary, and give them time to process and respond. Don't include roles in your response! \n Child's Input: "+input);
+                const response = result.response;
+                const text = response.text();
+                return text;
+            }
+
             //PROCESS TRANSCRIPT TODO
             console.log(transcript)
+            if (!transcript) return;
+            GenerateResponse(transcript).then((response)=>{
+                console.log(response)
+                setTTS(response);
+            })
         }
     },[props.micActive])
+
+    useEffect(()=>{
+        if(tts){
+            start();
+        }
+    },[tts])
 
     function SetCameraPosition(state: RootState) {
         if (!AnimCamera.current) return;
@@ -86,9 +121,6 @@ function Cafe(props:{micActive?:boolean})
             playAnim.current = false;
         }, 5100);
     }
-
-
-    const { transcript,resetTranscript } = useSpeechRecognition();
 
 
     return (
