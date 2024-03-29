02afe9a27f846bcf436f7b780538e59973406240
programmingninjas <programing.ninja0@gmail.com>
2024-03-29 19:13:07 +0530

    changes

--- client/src/pages/training/Metaverse.tsx
+++ client/src/pages/training/Metaverse.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import Navbar from '../../components/common/Navbar'
 import { Link } from 'react-router-dom'
 
 function Metaverse() {
   return (
     <>
-        <Link to={'/metaverse/dog'}>DOG</Link>
-        <Link to={'/metaverse/cafe'}>CAFE</Link>
+        <Navbar/>
+        <section className='container p-4 lg:px-64 mx-auto'>
+            <div className="flex gap-4 flex-wrap">
+                <Link to={'/metaverse/cafe'} className='shadow-xl bg-white flex gap-4 flex-col items-center justify-between h-96 w-full md:w-1/3 grow p-4 rounded-xl'>
+                    <div className='w-full grow rounded-lg overflow-clip'>
+                        <img src="../thumbnails/cafe.png" className='w-full h-full  rounded-xl' alt="Cafeverse" />
+                    </div>
+                    <h2 className='text-2xl font-bold'>Cafeverse</h2>
+                </Link>
+                <Link to={'/metaverse/dog'} className='shadow-xl bg-white flex gap-4 flex-col items-center justify-between h-96 w-full md:w-1/3 grow p-4 rounded-xl'>
+                    <div className='w-full grow rounded-lg overflow-clip'>
+                        <img src="../thumbnails/dog.png" className='w-full h-full  rounded-xl' alt="Petverse" />
+                    </div>
+                    <h2 className='text-2xl font-bold'>Petverse</h2>
+                </Link>
+            </div>
+        </section>
     </>
   )
 }
